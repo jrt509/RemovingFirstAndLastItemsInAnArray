e8c0cfb4f58934f2bfb9cae565e4ed37e172609d
RobT0509 <bischi8@gmail.com>
2020-06-04 22:33:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+array = ['topBun', 'pickle', 'hamburgerPatty', 'cheese', 'bottomBun']
+
+function itemRemover(array) {
+    array.pop(array)
+    array.reverse(array)
+
+    return array
+}
+
+console.log(itemRemover(array))
+console.log(itemRemover(array))
+console.log(itemRemover(array))
+console.log(itemRemover(array))
+console.log(itemRemover(array))
